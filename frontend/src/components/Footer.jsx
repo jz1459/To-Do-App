98233e129468dc0827c4ab79fd44b938c7e887e1
Jason Zheng <jason.zheng@yale.edu>
2023-07-27 14:41:05 -0400

    Added some comments + removed unused dependencies

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,6 @@ function Footer() {
     return (
         <section className="footer" id="footer">
             <Container>
-                {/* <div className="go-top"><a href="#about"><i className="bi bi-arrow-up-circle-fill"></i></a></div> */}
                     <Row className="align-items-center">
                         <Col size={12}>
                             <p>&copy; Jason Zheng. All Rights Reserved</p>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createRoot } from "react-dom/client";
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-// const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-// ReactDOM.render(<App />, document.getElementById("root"));
